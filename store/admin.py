8a6a35803d1978682b7e4b3752209d6abe62ff08
Cyril <ccyamoah@gmail.com>
2025-02-19 12:54:40 +0000

    initialize store_custom app with models, views, admin, and tests

--- store/admin.py
+++ store/admin.py
@@ -1,20 +1,40 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 
 from store.models import Address, Cart, CartItem, Collection, Customer, Order, OrderItem, Product
-from tags.models import Tag, TaggedItem
 from django.db.models import Count
 from django.utils.html import format_html
 from django.utils.http import urlencode
 from django.urls import reverse
 
 
+class InventoryFilter(admin.SimpleListFilter):
+    title = 'inventory'
+    parameter_name = 'inventory'
+
+    def lookups(self, request, model_admin):
+        return [
+            ('<10', 'Low'),
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value() == '<10':
+            return queryset.filter(inventory__lt=10)
+
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
+    actions = ['clear_inventory']
+    autocomplete_fields = ['collection']
+    prepopulated_fields = {
+        'slug': ['title']
+    }
     list_display = ['title', 'unit_price',
                     'inventory_status', 'collection_title']
     list_editable = ['unit_price']
+    list_filter = ['collection', 'last_update', InventoryFilter]
     list_per_page = 20
     list_select_related = ['collection']
+    search_fields = ['title']
 
     def collection_title(self, product):
         return product.collection.title
@@ -25,6 +45,12 @@ class ProductAdmin(admin.ModelAdmin):
             return 'Low'
         return 'OK'
 
+    @admin.action(description='Clear inventory')
+    def clear_inventory(self, request, queryset):
+        updated_count = queryset.update(inventory=0)
+        self.message_user(
+            request, f'{updated_count} products were successfully updated'), messages.SUCCESS
+
 
 @admin.register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
@@ -32,12 +58,12 @@ class CustomerAdmin(admin.ModelAdmin):
     list_editable = ['membership']
     list_per_page = 10
     ordering = ['first_name', 'last_name']
+    search_fields = ['first_name__istartswith', 'last_name__istartswith']
 
     @admin.display(ordering='orders_count')
     def orders_count(self, customer):
         url = f'{reverse('admin:store_order_changelist')}?{urlencode({'customer__id': str(customer.id)})}'
         return format_html("<a href={}>{}</a>", url, customer.orders_count)
-        return customer.orders_count
 
     def get_queryset(self, request):
         return super().get_queryset(request).annotate(
@@ -45,8 +71,18 @@ class CustomerAdmin(admin.ModelAdmin):
         )
 
 
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    autocomplete_fields = ['product']
+    extra = 0
+    min_num = 1
+    max_num = 10
+
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['customer']
+    inlines = [OrderItemInline]
     list_display = ['id', 'placed_at', 'payment_status', 'customer_name']
     list_select_related = ['customer']
 
@@ -58,6 +94,7 @@ class OrderAdmin(admin.ModelAdmin):
 @admin.register(Collection)
 class CollectionAdmin(admin.ModelAdmin):
     list_display = ['title', 'products_count']
+    search_fields = ['title']
 
     @admin.display(ordering='products_count')
     def products_count(self, collection):
@@ -70,9 +107,6 @@ class CollectionAdmin(admin.ModelAdmin):
         )
 
 
-admin.site.register(OrderItem)
-admin.site.register(Tag)
-admin.site.register(TaggedItem)
 admin.site.register(Address)
 admin.site.register(Cart)
 admin.site.register(CartItem)
