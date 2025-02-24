e22fcb02bf083809d4de61bd26dd7641c58b83e0
Cyril <ccyamoah@gmail.com>
2025-02-24 11:53:00 +0000

    refactor product views to use class-based views for improved structure and maintainability

--- store/urls.py
+++ store/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 
 urlpatterns = [
-    path('products/', views.product_list),
-    path('products/<int:id>/', views.product_detail),
+    path('products/', views.ProductList.as_view()),
+    path('products/<int:id>/', views.ProductDetail.as_view()),
     path('collection/', views.collection_list),
     path('collection/<int:pk>/', views.collection_detail, name='collection_detail'),
 ]
