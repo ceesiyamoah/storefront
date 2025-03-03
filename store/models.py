89ee8e5387064ef4122ccfa141f9eb4f272c173c
Cyril <ccyamoah@gmail.com>
2025-03-03 11:44:27 +0000

    refactor Customer model to use OneToOne relationship with User; remove redundant fields and update admin display; add permissions to Order model

--- store/models.py
+++ store/models.py
@@ -1,6 +1,8 @@
 from uuid import uuid4
 from django.db import models
 from django.core.validators import MinValueValidator
+from django.conf import settings
+from django.contrib import admin
 
 
 # Create your models here.
@@ -57,19 +59,26 @@ class Customer (models.Model):
         (MEMEMBERSHIP_GOLD, 'Gold'),
     ]
 
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    email = models.EmailField(unique=True)
     phone = models.CharField(max_length=15)
     birth_date = models.DateField(null=True)
     membership = models.CharField(
         max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMEMBERSHIP_BRONZE)
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'{self.first_name} {self.last_name}'
+        return f'{self.user.first_name} {self.user.last_name}'
+
+    @admin.display(ordering='user__first_name')
+    def first_name(self):
+        return self.user.first_name
+
+    @admin.display(ordering='user__first_name')
+    def last_name(self):
+        return self.user.last_name
 
     class Meta:
-        ordering = ['first_name', 'last_name']
+        ordering = ['user__first_name', 'user__last_name']
 
 
 class Order (models.Model):
@@ -88,6 +97,11 @@ class Order (models.Model):
     payment_status = models.CharField(
         max_length=1, choices=STATUS_CHOICES, default=STATUS_PENDING)
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+
+    class Meta:
+        permissions = [
+            ('cancel_order', 'Can Cancel order')
+        ]
 
 
 class OrderItem (models.Model):
