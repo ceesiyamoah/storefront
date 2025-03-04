568b4c38150756c3f90ea6b115b62427dde04c73
Cyril <ccyamoah@gmail.com>
2025-03-04 10:55:49 +0000

    add djoser for user authentication; implement custom UserCreateSerializer; update settings and URLs for JWT authentication

--- storefront/settings.py
+++ storefront/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'djoser',
     'playground',
     'debug_toolbar',
     'django_filters',
@@ -145,8 +146,21 @@ INTERNAL_IPS = [
 
 REST_FRAMEWORK = {
     'COERCE_DECIMAL_TO_STRING': False,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
 
+}
+SIMPLE_JWT = {
+    'AUTH_HEADER_TYPES': ('JWT',),
 }
 
 
 AUTH_USER_MODEL = 'core.User'
+
+
+DJOSER = {
+    'SERIALIZERS': {
+        'user_create': 'core.serializers.UserCreateSerializer'
+    }
+}
