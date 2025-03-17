b239658983af16a863770b3147772aa51f4e0553
Cyril <ccyamoah@gmail.com>
2025-03-17 16:58:12 +0000

    implement Celery task for notifying customers; update settings for Celery broker and beat schedule; refactor say_hello view

--- playground/views.py
+++ playground/views.py
@@ -1,26 +1,31 @@
 from django.shortcuts import render
-from django.core.mail import send_mail, mail_admins, BadHeaderError, EmailMessage
-from templated_mail.mail import BaseEmailMessage
+
+from playground.tasks import notify_customers
 
 
 def say_hello(request):
-
-    try:
-        # send_mail('Test Mail', 'this is the message on everything',
-        #           'info@cyrilyamoah.com', ['ccyamoah@gmail.com'])
-
-        # message = EmailMessage('Test Mail', 'this is the message on everything',
-        #                        'info@cyrilyamoah.com', ['ccyamoah@gmail.com'])
-        # message.attach_file('playground/static/images/pic.png')
-        # message.send()
-
-        message = BaseEmailMessage(
-            template_name='emails/hello.html',
-            context={'name': 'Cyril'}
-        )
-        message.send(['ccyamoah@gmail.com'])
-
-    except BadHeaderError:
-        pass
-
+    notify_customers.delay('Hello from the outside')
     return render(request, 'hello.html', {'name': 'Cyril', 'result': []})
+
+
+# def say_hello(request):
+
+#     try:
+#         # send_mail('Test Mail', 'this is the message on everything',
+#         #           'info@cyrilyamoah.com', ['ccyamoah@gmail.com'])
+
+#         # message = EmailMessage('Test Mail', 'this is the message on everything',
+#         #                        'info@cyrilyamoah.com', ['ccyamoah@gmail.com'])
+#         # message.attach_file('playground/static/images/pic.png')
+#         # message.send()
+
+#         message = BaseEmailMessage(
+#             template_name='emails/hello.html',
+#             context={'name': 'Cyril'}
+#         )
+#         message.send(['ccyamoah@gmail.com'])
+
+#     except BadHeaderError:
+#         pass
+
+#     return render(request, 'hello.html', {'name': 'Cyril', 'result': []})
--- storefront/settings.py
+++ storefront/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from datetime import timedelta
 import os
 from pathlib import Path
+from celery.schedules import crontab
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -186,3 +187,15 @@ EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 2525
 DEFAULT_FROM_EMAIL = 'account@cyrilyamoah.com'
+
+
+CELERY_BROKER_URL = 'redis://localhost:6379/1'
+
+CELERY_BEAT_SCHEDULE = {
+    'notify_customers': {
+        'task': "playground.tasks.notify_customers",
+        'schedule': 5,
+        'args': ['Hello World'],
+
+    }
+}
