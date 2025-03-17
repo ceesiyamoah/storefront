fe05d1c84ce4685b2bedfae40640fe3d30909030
Cyril <ccyamoah@gmail.com>
2025-03-17 11:38:49 +0000

    refactor email sending in say_hello view; switch to EmailMessage and attach a file

--- playground/views.py
+++ playground/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
-from django.core.mail import send_mail, mail_admins, BadHeaderError
+from django.core.mail import send_mail, mail_admins, BadHeaderError, EmailMessage
 
 
 def say_hello(request):
 
     try:
-        send_mail('Test Mail', 'this is the message on everything',
-                  'info@cyrilyamoah.com', ['ccyamoah@gmail.com'])
+        # send_mail('Test Mail', 'this is the message on everything',
+        #           'info@cyrilyamoah.com', ['ccyamoah@gmail.com'])
+        message = EmailMessage('Test Mail', 'this is the message on everything',
+                               'info@cyrilyamoah.com', ['ccyamoah@gmail.com'])
+        message.attach_file('playground/static/images/pic.png')
+        message.send()
 
     except BadHeaderError:
         pass
