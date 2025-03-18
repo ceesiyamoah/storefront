5ab07ec51ed5e7af050cf4b7fb4647cd26fe8570
Cyril <ccyamoah@gmail.com>
2025-03-18 16:56:35 +0000

    update .gitignore, configure production settings for Whitenoise, and add requirements.txt for dependencies

--- storefront/settings/prod.py
+++ storefront/settings/prod.py
@@ -5,4 +5,13 @@ from storefront.settings.common import *
 DEBUG = False
 SECRET_KEY = os.environ['SECRET_KEY']
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['storefront-kd93.onrender.com']
+
+INSTALLED_APPS += ["whitenoise.runserver_nostatic"]
+MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")
+
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+# Optional: Enable compression and caching
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
